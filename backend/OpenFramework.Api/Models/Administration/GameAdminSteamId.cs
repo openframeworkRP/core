d71308586898a54aea5090a0e9f7b4c6cae3e70b
Ben 10 <benpaulingpt@gmail.com>
2026-05-02 15:17:14 +0200

    fix(core-api): ajouter [Key] sur GameAdminSteamId.SteamId (EF Core ne detecte pas la PK automatiquement)

--- backend/OpenFramework.Api/Models/Administration/GameAdminSteamId.cs
+++ backend/OpenFramework.Api/Models/Administration/GameAdminSteamId.cs
@@ -1,6 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace OpenFramework.Api.Models.Administration;
 
 public class GameAdminSteamId
 {
+    [Key]
     public string SteamId { get; set; } = "";
 }
